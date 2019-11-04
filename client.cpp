a0d2bfdc39d43d24f2ed34b7c3e6021196581b83
Sanyam Phirani <sanyamphirani@Sanyams-MacBook-Pro.local>
2019-11-03 19:13:53 -0600

    client modification

--- client.cpp
+++ client.cpp
@@ -13,40 +13,46 @@ int getServerPortNumber();
 string getServerHostName();
 
 int main(int argc, char *argv[]) {
+
     int portNumber = getServerPortNumber();
     string hostName = getServerHostName();
 
     int sockfd, portno, n;
 
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in serverAddress;
     struct hostent *server;
 
-    char buffer[256];
+    string email = "";
+    char buffer[1024];
     portno = portNumber;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         perror("ERROR opening socket");
     server = gethostbyname(hostName.c_str());
+
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        cerr << "ERROR, no such host\n";
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
+
+    bzero((char *) &serverAddress, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&serverAddress.sin_addr.s_addr,
          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+    serverAddress.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serverAddress,sizeof(serverAddress)) < 0) {
         perror("ERROR connecting");
+        exit(0);
+    }
+
+    bzero(buffer,1024);
     printf("Please enter the email: ");
-    bzero(buffer,256);
-    cin >> buffer;
-    n = write(sockfd,buffer,strlen(buffer));
+    cin >> email;
+    n = write(sockfd, email.c_str(), email.size());
     if (n < 0)
          perror("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
+    bzero(buffer,1024);
+    n = read(sockfd,buffer,1024);
     if (n < 0)
          perror("ERROR reading from socket");
     printf("%s\n",buffer);
@@ -55,7 +61,7 @@ int main(int argc, char *argv[]) {
 
 /**
     prompts the "user" to enter the server's port number
-    to which the client wishes to make connection to
+    to which the client wishes to make connection with
     @ returns the entered port number
 **/
 int getServerPortNumber() {
@@ -73,6 +79,11 @@ int getServerPortNumber() {
     return portNumber;
 }
 
+/**
+    prompts the "user" get the server's name
+    in our case it is localhost
+    @returns the entered hostname
+**/
 string getServerHostName() {
     string hostName = "";
     cout << "Enter server host name: ";
